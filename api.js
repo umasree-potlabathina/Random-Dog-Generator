3669b09d580493b615dd2c05d6e3e54e2b90ef47
umasree-potlabathina <105449679+umasree-potlabathina@users.noreply.github.com>
2023-03-09 13:24:23 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,54 @@
+const dogDiv = document.getElementById('dogDiv');
+const dogButton = document.getElementById('dogButton');
+const changeMode = document.getElementById('modeButton');
+
+const getNewDog = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(json => {
+    dogDiv.innerHTML = `<img src='${json.message}' height=150 width=auto/>`
+})
+}
+
+dogButton.onclick = () => getNewDog();
+
+// default theme:
+
+    // body
+        const body = document.getElementById('body');
+
+        body.classList.add('lightBackground')
+
+    // buttons
+        const newDogButton = document.getElementById('dogButton');
+        const darkButton = document.getElementById('darkmodeButton');
+        const lightButton = document.getElementById('lightmodeButton');
+;
+        newDogButton.classList.add('lightButton');
+        darkButton.classList.add('lightButton');
+        lightButton.classList.add('lightButton');
+
+// Theme changing:
+
+// Dark Mode
+const darkMode = () => {
+    // background
+    body.classList.remove('lightBackground');
+    body.classList.add('darkBackground');
+
+    // buttons
+}
+
+darkButton.onclick = () => darkMode();
+
+
+// Light Mode
+
+
+const lightMode = () => {
+    body.classList.add('lightBackground')
+
+    // buttons
+}
+
+lightButton.onclick = () => lightMode();
